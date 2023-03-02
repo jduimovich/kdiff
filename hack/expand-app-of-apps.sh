5a6cae90093fb3a83842badd29228a572812a462
John Duimovich <jduimovich@gmail.com>
2023-03-02 09:09:46 -0500

    reorg hack scripts

--- hack/expand-app-of-apps.sh
+++
@@ -1,5 +0,0 @@
-# this will be used to find the App Sets 
-# to expand and print the expanded gitops manifests.
-
-kustomize build infra-deployments/argo-cd-apps/overlays/staging 
-yq app-of-apps.yaml -o=json > app-of-apps.json 
--- hack/reset.sh
+++ hack/reset.sh
@@ -1,9 +1,6 @@
 rm -rf  kustomized-cache
-mkdir -p kustomized-cache
-
+mkdir -p kustomized-cache 
 rm -rf  change-history
-mkdir -p change-history
-
-rm -rf infra-deployments
-git clone  https://github.com/redhat-appstudio/infra-deployments.git
+mkdir -p change-history 
+rm -rf infra-deployments 
 
